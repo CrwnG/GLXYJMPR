f43187648e580941af95381ae53bb57f0b0c0422
CrwnG <131405117+CrwnG@users.noreply.github.com>
2025-02-27 22:43:57 -0800

    Update SoundManager.swift

--- SoundManager.swift
+++ SoundManager.swift
@@ -5,7 +5,6 @@ class SoundManager: ObservableObject {
     private var audioPlayer: AVAudioPlayer?
     
     func playSound(_ soundName: String) {
-        // Agrega "menu.mp3" y "jump.wav" en Resources
         guard let url = Bundle.main.url(forResource: soundName, withExtension: nil) else {
             print("No se encontró el sonido: \(soundName)")
             return
