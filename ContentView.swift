f22d019c5b396e5bb49ffe6107c64de1307a9215
CrwnG <131405117+CrwnG@users.noreply.github.com>
2025-02-27 22:35:47 -0800

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var currentScreen: ScreenState = .menu
     @State private var selectedImage: UIImage? = nil
-
+    
     var body: some View {
         switch currentScreen {
         case .menu:
@@ -12,10 +12,9 @@ struct ContentView: View {
             AvatarCreationView(selectedImage: $selectedImage, currentScreen: $currentScreen)
         case .game:
             if selectedImage == nil {
-                // Si no hay avatar, forzar creación
                 AvatarCreationView(selectedImage: $selectedImage, currentScreen: $currentScreen)
             } else {
-                GameView(selectedImage: selectedImage)
+                GameView(selectedImage: selectedImage, currentScreen: $currentScreen)
             }
         case .highscore:
             HighscoreView(currentScreen: $currentScreen)
