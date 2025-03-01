020a36f4e1451e6725c3f96232f32cb8353ba218
CrwnG <131405117+CrwnG@users.noreply.github.com>
2025-03-01 11:59:55 -0800

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,23 +1,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var currentScreen: ScreenState = .menu
     @State private var selectedImage: UIImage? = nil
     
     var body: some View {
-        switch currentScreen {
-        case .menu:
-            MenuView(currentScreen: $currentScreen, selectedImage: $selectedImage)
-        case .avatarCreation:
-            AvatarCreationView(selectedImage: $selectedImage, currentScreen: $currentScreen)
-        case .game:
-            if selectedImage == nil {
-                AvatarCreationView(selectedImage: $selectedImage, currentScreen: $currentScreen)
-            } else {
-                GameView(selectedImage: selectedImage, currentScreen: $currentScreen)
-            }
-        case .highscore:
-            HighscoreView(currentScreen: $currentScreen)
+        NavigationStack {
+                MenuView(selectedImage: $selectedImage)
         }
     }
 }
